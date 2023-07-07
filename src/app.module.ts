331a92fae2b224aab2b4e84b47b761b0f6e6bbaa
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-07-07 15:31:48 -0500

    Agrega soporte pagina estatica

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeopleModule } from './people/people.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PeopleModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PeopleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/people/dto/create-person.dto.ts
+++ src/people/dto/create-person.dto.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreatePersonDto {
+  @ApiProperty({
+    type: String,
+    description: 'name',
+    example: 'Antonio',
+  })
   name: string;
 }
