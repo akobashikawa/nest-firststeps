c692b5780d3f40334b130766e72066527424a0cf
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-07-09 22:00:15 -0500

    Configura vue como client y simplifica

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld msg="You did it!" />
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
